a9fa5763fe28214e0ed922dc53ed150ce5117611
Lou Andria <lou@hei.school>
2023-08-04 13:43:19 +0200

    refactor: Group become record

--- src/test/java/school/hei/linearP/hei/costly/Group.java
+++ src/test/java/school/hei/linearP/hei/costly/Group.java
@@ -1,32 +1,20 @@
 package school.hei.linearP.hei.costly;
 
-public class Group extends CostlyBounderValue<Group> {
+import school.hei.linearE.instantiableE.BounderValue;
 
-  private static final double DEFAULT_COST_WEIGHT = 1;
-  private final String name;
-  private final double cost;
-
-  private Group(String name, double cost) {
-    this.name = name;
-    this.cost = cost;
-  }
+public record Group(String name) implements BounderValue<Group> {
 
   public static Group g1() {
-    return new Group("g1", 1);
+    return new Group("g1");
   }
 
   public static Group g2() {
-    return new Group("g2", 2);
+    return new Group("g2");
   }
 
 
   public static Group[] values() {
     return new Group[]{g1(), g2()};
-  }
-
-  @Override
-  public double cost() {
-    return DEFAULT_COST_WEIGHT * cost;
   }
 
   @Override
