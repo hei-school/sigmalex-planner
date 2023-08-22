f082ef0dee22c999085d1e7becc118b9cbe05fd3
Lou Andria <lou@hei.school>
2023-08-22 17:34:21 +0300

    feat: dichotomy on Add has little to no effect

--- src/main/java/school/hei/sigmalex/linearE/Add.java
+++ src/main/java/school/hei/sigmalex/linearE/Add.java
@@ -1,22 +1,64 @@
 package school.hei.sigmalex.linearE;
 
+import lombok.SneakyThrows;
+import school.hei.sigmalex.concurrency.Workers;
 import school.hei.sigmalex.linearE.instantiableE.AddIE;
+import school.hei.sigmalex.linearE.instantiableE.Constant;
 import school.hei.sigmalex.linearE.instantiableE.InstantiableE;
 import school.hei.sigmalex.linearE.instantiableE.SubstitutionContext;
 import school.hei.sigmalex.linearE.instantiableE.Variable;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Set;
-import java.util.stream.Stream;
 
 import static java.util.stream.Collectors.toSet;
 import static school.hei.sigmalex.linearE.instantiableE.Constant.ZERO;
 import static school.hei.sigmalex.linearE.instantiableE.IEFactory.addie;
 
-public record Add(LinearE le1, LinearE le2) implements LinearE {
+public record Add(List<LinearE> leList) implements LinearE {
 
   @Override
   public NormalizedLE normalize(SubstitutionContext substitutionContext) {
+    return normalizeDichotomically(substitutionContext);
+  }
+
+  @SneakyThrows
+  private NormalizedLE normalizeDichotomically(SubstitutionContext substitutionContext) {
+    var leListSize = leList.size();
+    if (leListSize == 0) {
+      return new NormalizedLE(0);
+    }
+    if (leListSize == 1) {
+      return leList.get(0).normalize(substitutionContext);
+    }
+    if (leListSize == 2) {
+      return normalize(leList.get(0), leList.get(1), substitutionContext);
+    }
+
+    var futureOperand1 = Workers.submit(() ->
+        new Add(leList.subList(0, leListSize / 2)).normalize(substitutionContext));
+    var futureOperand2 = Workers.submit(() ->
+        new Add(leList.subList(leListSize / 2, leListSize)).normalize(substitutionContext));
+    return normalize(futureOperand1.get(), futureOperand2.get());
+  }
+
+  private NormalizedLE normalize(NormalizedLE normalized1, NormalizedLE normalized2) {
+    var weightedV = new HashMap<>(normalized1.weightedV());
+    normalized2.weightedV().forEach((v, e) -> {
+      if (weightedV.containsKey(v)) {
+        weightedV.put(v, new Constant<>(weightedV.get(v).simplify() + e.simplify()));
+      } else {
+        weightedV.put(v, e);
+      }
+    });
+
+    var e = new Constant<>(normalized1.e().simplify() + normalized2.e().simplify());
+
+    return new NormalizedLE(weightedV, e);
+  }
+
+  private NormalizedLE normalize(LinearE le1, LinearE le2, SubstitutionContext substitutionContext) {
     var normalizedLe1 = le1.normalize(substitutionContext);
     var normalizedLe2 = le2.normalize(substitutionContext);
     var weightedV1 = normalizedLe1.weightedV();
@@ -35,9 +77,6 @@ public record Add(LinearE le1, LinearE le2) implements LinearE {
 
   @Override
   public Set<Variable> variables() {
-    return Stream.concat(
-        le1.variables().stream(),
-        le2.variables().stream()
-    ).collect(toSet());
+    return leList.stream().flatMap(le -> le.variables().stream()).collect(toSet());
   }
 }
