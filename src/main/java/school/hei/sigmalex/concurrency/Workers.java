a97007b8d95093adf9159c56c5bae7789cea4600
Lou Andria <lou@hei.school>
2023-08-24 11:06:07 +0300

    conf: use 100 threads

--- src/main/java/school/hei/sigmalex/concurrency/Workers.java
+++ src/main/java/school/hei/sigmalex/concurrency/Workers.java
@@ -17,7 +17,7 @@ public class Workers {
   private static int totalSubmittedTasks = 0;
 
   private static ExecutorService newThreadPool() {
-    return newWorkStealingPool(10);
+    return newWorkStealingPool(100);
   }
 
   @SneakyThrows
